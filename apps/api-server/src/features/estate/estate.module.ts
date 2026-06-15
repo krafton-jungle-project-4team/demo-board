92bb46823a5cc91a5d9b5a433ecacec7f86ff385
sijun-yang <yangsijun5528@gmail.com>
2026-06-15 16:21:49 +0900

    feat: estate_properties 엔티티와 더미데이터 sql 스크립트 추가

--- apps/api-server/src/features/estate/estate.module.ts
+++ apps/api-server/src/features/estate/estate.module.ts
@@ -1,11 +1,11 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { EstateAiController } from "./controller/estate-ai.controller";
-import { EstateTransactionEntity } from "./database";
+import { EstatePropertyEntity, EstateTransactionEntity } from "./database";
 import { EstateAiQueryService } from "./service/estate-ai-query.service";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([EstateTransactionEntity])],
+    imports: [TypeOrmModule.forFeature([EstatePropertyEntity, EstateTransactionEntity])],
     controllers: [EstateAiController],
     providers: [EstateAiQueryService]
 })
